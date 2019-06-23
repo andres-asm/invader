174ea39e7e7a69919e240f930e1f126903bdd777
radius <andres.430@gmail.com>
2019-06-23 14:33:25 -0500

    Separate gui and renderer

---
+++ src/frontend/common.h
@@ -0,0 +1,27 @@
+/* Common defines, includes and variables for all implementations */
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+
+#include "nuklear.h"
+
+#define UNUSED(a) (void)a
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) < (b) ? (b) : (a))
+#define LEN(a) (sizeof(a)/sizeof(a)[0])
+
+#define MAX_SIZE 256
+#define MAX_VERTEX_MEMORY 512 * 1024
+#define MAX_ELEMENT_MEMORY 128 * 1024
+
+struct nk_context *ctx;
+struct nk_colorf bg;
+
+#endif
--- src/frontend/invader_sdl2.c
+++ src/frontend/invader_sdl2.c
@@ -9,13 +9,32 @@
 #include <limits.h>
 #include <time.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <stdbool.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define NK_IMPLEMENTATION
+#define NK_SDL_GL3_IMPLEMENTATION
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
 #include "invader.h"
-
-#include "nuklear.h"
 #include "nuklear_sdl_gl3.h"
 
 #define WINDOW_WIDTH 1920
@@ -28,10 +47,6 @@ int main(int argc, char *argv[])
    SDL_GLContext glContext;
    int win_width, win_height;
    int running = 1;
-
-   /* GUI */
-   struct nk_context *ctx;
-   struct nk_colorf bg;
 
    /* SDL setup */
    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0");
@@ -72,45 +87,14 @@ int main(int argc, char *argv[])
       {
          /* Input */
          SDL_Event evt;
+         render_gui(ctx);
          nk_input_begin(ctx);
-         while (SDL_PollEvent(&evt)) {
+         while (SDL_PollEvent(&evt))
+         {
             if (evt.type == SDL_QUIT) goto cleanup;
             nk_sdl_handle_event(&evt);
-         } nk_input_end(ctx);
-
-         /* GUI */
-         if (nk_begin(ctx, "Not the final GUI", nk_rect(50, 50, 230, 250),
-            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-         {
-            enum {EASY, HARD};
-            static int op = EASY;
-            static int property = 20;
-
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-               printf("button pressed!\n");
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-            nk_layout_row_dynamic(ctx, 22, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-                  nk_layout_row_dynamic(ctx, 120, 1);
-                  bg = nk_color_picker(ctx, bg, NK_RGBA);
-                  nk_layout_row_dynamic(ctx, 25, 1);
-                  bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-                  bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-                  bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-                  bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-                  nk_combo_end(ctx);
-            }
          }
-         nk_end(ctx);
+         nk_input_end(ctx);
 
          /* Draw */
          SDL_GetWindowSize(win, &win_width, &win_height);
