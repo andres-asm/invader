2db1df2f1e2db87f81f93228c74b9c7380d08544
radius <andres.430@gmail.com>
2019-06-24 23:01:41 -0500

    add missing files

---
+++ src/nuklear_extra.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <compat/strl.h>
+
+#include "invader.h"
+#include "config.h"
+
+bool nk_checkbox_bool(struct nk_context* ctx, const char* label, bool *active)
+{
+   int    x = *active;
+   bool ret = nk_checkbox_label(ctx, label, &x);
+   *active  = x;
+
+   return ret;
+}
---
+++ src/nuklear_extra.h
@@ -0,0 +1,6 @@
+#ifndef NUKLEAR_EXTRA_H_
+#define NUKLEAR_EXTRA_H_
+
+bool nk_checkbox_bool(struct nk_context* ctx, const char* label, bool *active);
+
+#endif
