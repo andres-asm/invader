e7e078915525dbde3aca4dc5f73b76f739b402e1
radius <andres.430@gmail.com>
2020-04-01 00:56:57 -0500

    add dummy localization file

---
+++ src/localization.c
@@ -0,0 +1,30 @@
+#include "config.h"
+
+/* dummy function, lists all the localization strings */
+static void localization_strings()
+{
+
+   /* categories*/
+   __("setting_categories_none");
+   __("setting_categories_general");
+   __("setting_categories_video");
+   __("setting_categories_audio");
+   __("setting_categories_input");
+
+   /* settings */
+   /* directory_cores */
+   __("directory_cores_label");
+   __("directory_cores_desc");
+
+   /* directory_games */
+   __("directory_games_label");
+   __("directory_games_desc");
+
+      /* video_vsync */
+   __("video_vsync_label");
+   __("video_vsync_desc");
+
+      /* video_fullscreen */
+   __("video_fullscreen_label");
+   __("video_fullscreen_desc");
+}
