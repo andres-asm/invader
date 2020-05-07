d5da33ff26953042346e51c5ecbcf8a2f9f4a3b5
radius <andres.430@gmail.com>
2020-05-07 15:46:57 -0500

    [kami] implement the changes on the latest backend, create function for all the frontend callbacks

--- src/frontend/imgui/kami_imgui_sdl2.cpp
+++ src/frontend/imgui/kami_imgui_sdl2.cpp
@@ -395,9 +395,8 @@ void Kami::Main(const char* title)
                if (ImGui::Button(_("core_current_start_core_label"), ImVec2(120, 0)))
                {
                   piccolo->unload_core();
-                  piccolo->load_core(core_info->file_name, frontend_supports_bitmasks);
-                  piccolo->set_input_poll_callback(InputPoll);
-                  piccolo->load_game(NULL);
+                  piccolo->load_game(core_info->file_name, NULL, frontend_supports_bitmasks);
+                  piccolo->set_callbacks(InputPoll);
                   core_info = piccolo->get_info();
                }
                tooltip(_("core_current_start_core_desc"));
@@ -417,9 +416,8 @@ void Kami::Main(const char* title)
             {
                piccolo->unload_core();
                core_info = &core_info_list[current_core];
-               piccolo->load_core(core_info->file_name, frontend_supports_bitmasks);
-               piccolo->set_input_poll_callback(InputPoll);
-               piccolo->load_game(content_file_name);
+               piccolo->set_callbacks(InputPoll);
+               piccolo->load_game(core_info->file_name, content_file_name, frontend_supports_bitmasks);
                core_info = piccolo->get_info();
             }
 #ifdef DEBUG
